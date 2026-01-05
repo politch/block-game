1b2dcdc29d8e06d3b7987c59d061e717a0be1673
Victor-Stefan Arseniu <victor.arseniu@gmail.com>
2026-01-05 19:17:42 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <GLFW/glfw3.h>
+
+const uint32_t kWidth = 512;
+const uint32_t kHeight = 512;
+
+void Start()
+{
+	if (!glfwInit()) {
+		return;
+	}
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow *window = glfwCreateWindow(kWidth, kHeight, "WebGPU window",
+					      nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		// TODO: Render a triangle using WebGPU.
+	}
+}
+
+int main()
+{
+	Start();
+}
