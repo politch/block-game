4f6137d4a372ba745f163ef95c41b2acc8bf1efb
Victor-Stefan Arseniu <victor.arseniu@gmail.com>
2026-01-06 21:12:20 +0200

    Fixed: build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+cd /cache || exit
+
 mkdir -p build
 cd build || exit
-emcmake cmake -G Ninja ..
+emcmake cmake -G Ninja /src/
 ninja
 
 mkdir -p /out
 cp src/BlockGame.js /out/
 cp src/BlockGame.wasm /out/
 cp src/BlockGame.data /out/
-cp ../main.html /out/
-cp ../image.png /out/
+cp /src/main.html /out/
+cp /src/image.png /out/
